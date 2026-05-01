0f5971b95b925ee57f32e4caee84a989f33d154d
Aakash Sriram <107379870+aakashsriram1@users.noreply.github.com>
2026-05-01 11:53:25 -0700

    removing footer no need

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 import "../styles/globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
@@ -20,7 +19,6 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
